a3e7fcaa6d5b54b97fd18d1d5550a35a20282590
Tarek Khalil <tarek.saber.se@gmail.com>
2014-11-21 11:26:19 +0400

    added fetching commits

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
-
 // main application module
-var appModule = angular.module('githubApp', []);
+var appModule = angular.module('githubApp', ['ngRoute']);
 
-appModule.controller('githubController',
+appModule.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'views/home.html',
+            controller: 'homeController'
+        })
+        .when('/commit/:userName/:repoName', {
+            templateUrl: 'views/commits.html',
+            controller: 'commitController'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+});
+
+appModule.controller('commitController',
+    function ($scope, gitFetcherService, $routeParams) {
+        $scope.repoName = $routeParams.repoName;
+        $scope.userName = $routeParams.userName;
+
+        gitFetcherService
+            .fetchCommits($scope.userName, $scope.repoName)
+            .success(function (result) {
+                $scope.commits = result;
+            });
+    });
+
+appModule.controller('homeController',
     function ($scope, gitFetcherService) {
 
         $scope.appTitle = 'Github Repos Explorer';
@@ -14,7 +40,7 @@ appModule.controller('githubController',
 
             // fetching the repos using the fetcher service
             gitFetcherService
-                .fetch($scope.repoName)
+                .fetchRepos($scope.userName)
                 .success(function (result) {
                     $scope.repos = result;
 
@@ -28,10 +54,17 @@ appModule.controller('githubController',
 
 appModule.service('gitFetcherService',
     function ($http) {
-        this.fetch = function (repoName) {
+        this.fetchRepos = function (userName) {
             return $http({
                 method: 'Get',
-                url: 'https://api.github.com/users/' + repoName + '/repos'
+                url: 'https://api.github.com/users/' + userName + '/repos'
+            });
+        };
+
+        this.fetchCommits = function (userName, repoName) {
+            return $http({
+                method: 'Get',
+                url: 'https://api.github.com/repos/' + userName + '/' + repoName + '/commits'
             });
         };
     });
